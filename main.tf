dc92c1f57adc9fd3844a49d19b737022a0a62044
saadhusain-hashicorp <176054127+saadhusain-hashicorp@users.noreply.github.com>
2025-05-28 18:44:02 -0400

    corrected security group id name

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
 
- vpc_security_groups_ids = [aws_security_group.blog.id]
+ vpc_security_group_ids = [aws_security_group.blog.id]
 
   tags = {
     Name = var.instance_name
